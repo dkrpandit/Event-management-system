21372e3b9df6575f4553a4951e6b964fb4a53c8b
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-04-19 15:13:12 +0530

    Day 4

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$FirstName = $_POST['FirstName'];
+$LastName = $_POST['LastName'];
+$Branch = $_POST['Branch'];
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+$email = $_POST['email'];
+$contactNo = $_POST['contactNo'];
+// Database connection
+?>
