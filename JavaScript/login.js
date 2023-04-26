5b8dc161293ee0e94517f018c6658d62a77b7726
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-04-27 00:10:30 +0530

    Day 9

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,29 +1,11 @@
-const form = document.getElementById('form1');
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const loginButton = document.getElementById('button3');
-
-// Function to check if the form is valid
-// function isFormValid() {
-//   return usernameInput.value.trim() !== '' && passwordInput.value.trim() !== '';
-// }
-
-// // Function to enable or disable the login button based on form validity
-// function toggleLoginButton() {
-//   loginButton.disabled = !isFormValid();
-// }
-
-// // Call the toggleLoginButton function on page load
-// toggleLoginButton();
-
-// // Call the toggleLoginButton function on input change
-// form.addEventListener('input', toggleLoginButton);
-
-// if(usernameInput==null||passwordInput==null)
-// {
-// //   loginButton.disabled=true;
-//   loginButton.ariaDisabled=true;
-// }
-// else{
-//     loginButton.disabled=true;
-// }
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("form1");
+  form.addEventListener("submit", function(event) {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "" || password === "") {
+      alert("Please fill out all fields before submitting the form.");
+      event.preventDefault();
+    }
+  });
+});
