d5ae2c14c88f769f5c6b86b765c369a90a5b16d1
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-05-21 00:20:33 +0530

    Day 14

--- Cricket.php
+++ Cricket.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cricket</title>
     <link rel="stylesheet" href="CSS/home.css">
     <link rel="stylesheet" href="CSS/login.css">
     <script src="JavaScript/login.js"></script>
