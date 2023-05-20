cf17c8bacab4ddd14aad9c263a2cbd8f5aa6131c
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-05-20 13:00:26 +0530

    Day 13

--- Registration.php
+++ Registration.php
@@ -3,51 +3,67 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $insert = false;
+$usernameTaken = false; // Add a new variable to track if the username is already taken
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Form has been submitted
-  $server = "localhost";
-  $username = "pandit";
-  $password = "DharmP1234$";
-  $dbname = "pbl";
+    // Form has been submitted
+    $server = "localhost";
+    $username = "pandit";
+    $password = "DharmP1234$";
+    $dbname = "pbl";
 
-  // Create a database connection
-  $con = mysqli_connect($server, $username, $password, $dbname);
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password, $dbname);
 
-  // Check for connection success
-  if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
+    // Check for connection success
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-  // Prepare the SQL statement
-  $stmt = mysqli_prepare($con, "INSERT INTO `pbl`.`Registration` (`Username`, `FirstName`, `LastName`, `Branch`,`year`, `Password2`, `email`, `contactNo`,`DateTime`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())");
+    // Prepare the SQL statement to check if the username already exists
+    $checkStmt = mysqli_prepare($con, "SELECT * FROM `Registration` WHERE `Username` = ?");
+    mysqli_stmt_bind_param($checkStmt, "s", $_POST['Username']);
+    mysqli_stmt_execute($checkStmt);
 
-  // Bind the parameters
-  $username = isset($_POST['Username']) ? $_POST['Username'] : '';
-  $firstName = isset($_POST['FirstName']) ? $_POST['FirstName'] : '';
-  $lastName = isset($_POST['LastName']) ? $_POST['LastName'] : '';
-  $branch = isset($_POST['Branch']) ? $_POST['Branch'] : '';
-  $year = isset($_POST['year']) ? $_POST['year'] : '';
-  $password2 = isset($_POST['Password2']) ? $_POST['Password2'] : '';
-  $email = isset($_POST['email']) ? $_POST['email'] : '';
-  $contactNo = isset($_POST['contactNo']) ? $_POST['contactNo'] : '';
+    // Check if the username already exists
+    if (mysqli_stmt_fetch($checkStmt)) {
+        $usernameTaken = true;
+    } else {
+        // Prepare the SQL statement for inserting the new user
+        $insertStmt = mysqli_prepare($con, "INSERT INTO `pbl`.`Registration` (`Username`, `FirstName`, `LastName`, `Branch`,`year`, `Password2`, `email`, `contactNo`,`DateTime`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())");
 
-  mysqli_stmt_bind_param($stmt, "ssssssss", $username, $firstName, $lastName, $branch, $year, $password2, $email, $contactNo);
+        // Bind the parameters
+        $username = isset($_POST['Username']) ? $_POST['Username'] : '';
+        $firstName = isset($_POST['FirstName']) ? $_POST['FirstName'] : '';
+        $lastName = isset($_POST['LastName']) ? $_POST['LastName'] : '';
+        $branch = isset($_POST['Branch']) ? $_POST['Branch'] : '';
+        $year = isset($_POST['year']) ? $_POST['year'] : '';
+        $password2 = isset($_POST['Password2']) ? $_POST['Password2'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $contactNo = isset($_POST['contactNo']) ? $_POST['contactNo'] : '';
 
-  // Execute the statement
-  if (mysqli_stmt_execute($stmt)) {
-    $insert = true;
-  }
+        mysqli_stmt_bind_param($insertStmt, "ssssssss", $username, $firstName, $lastName, $branch, $year, $password2, $email, $contactNo);
 
-  // Close the statement and the database connection
-  mysqli_stmt_close($stmt);
-  mysqli_close($con);
+        // Execute the statement
+        if (mysqli_stmt_execute($insertStmt)) {
+            $insert = true;
+        }
+
+        // Close the insert statement
+        mysqli_stmt_close($insertStmt);
+    }
+
+    // Close the check statement and the database connection
+    mysqli_stmt_close($checkStmt);
+    mysqli_close($con);
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
+    <!-- Head content -->
+    <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registration</title>
@@ -57,7 +73,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
-  <a href="https://www.isquareit.edu.in/" target="_blank"> <img class="head12" src="images/i2it.png" alt=""></a>
+    <!-- HTML body content -->
+    <a href="https://www.isquareit.edu.in/" target="_blank"> <img class="head12" src="images/i2it.png" alt=""></a>
   <header>
     <nav class="navbar">
       <ul>
@@ -77,12 +94,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </nav>
   </header>
   <div class="container">
-  <?php
+    <?php
     if ($insert == true) {
-      echo '<div class="success">Registration is successfully please go back to home page and join respective events</div> <br>';
+        echo '<div class="success">Registration is successful. Please go back to the home page and join respective events.</div><br>';
+    } elseif ($usernameTaken) {
+        echo '<div class="error">Username is already taken. Please choose a different username.</div><br>';
     }
     ?>
 
+    <!-- Rest of the HTML content -->
     <form action="Registration.php" method="post" id="contact_form" onsubmit="return validateForm()">
       <legend>
         <h2><b>Registration Form</b></h2>
